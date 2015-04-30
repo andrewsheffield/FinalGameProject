a105b15e9f9124fb84d484f835c6d3de5ce5dd6e
ajsheffield <andrew.sheffield@sv-code.com>
2015-04-30 11:04:33 -0700

    Adding game assets for first playable version

---
+++ Assets/Scripts/game_control.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+static var score : int = 0;
+
+var scoreText : TextMesh;
+
+private var scoreSave : int = 0;
+
+function Start () {
+
+}
+
+function Update () {
+	if (score != scoreSave) {
+		scoreSave = score;
+		Debug.Log(score);
+		scoreText.text = "Score: " + score;
+	}
+}
---
+++ Assets/Scripts/torpedo_controller.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var speed : float =  5;
+
+function Start () {
+	GetComponent.<Rigidbody2D>().velocity.y = -speed;
+}
+
+function Update () {
+
+	if (transform.position.y < -4.39) {
+		Destroy(this.gameObject);
+	}
+
+}
