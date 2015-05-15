70b141e429c598605f018dca46845cfc8dd28834
ajsheffield <andrew.sheffield@sv-code.com>
2015-05-15 11:39:23 -0700

    Added a progress bar on the bottom of the screen

---
+++ BoatGame/Assets/Scripts/progressBarScript.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var progressShip : GameObject;
+
+function Update () {
+
+	var distancePercent : float = 100f / 30000f * game_control.distance;
+	var barWidth : float = GetComponent.<SpriteRenderer>().bounds.size.x;
+	var newXPos : float = barWidth / 100f * distancePercent;
+	Debug.Log(newXPos);
+	
+	progressShip.transform.localPosition.x = newXPos;
+}
